0f99b5fcf12c171b155a16e72ea1bb5936d74297
FinnPenn <finnpenning@hotmail.de>
2023-10-06 23:16:03 +0200

    feat(media): add grid layout

--- pages/mediathek.tsx
+++ pages/mediathek.tsx
@@ -4,7 +4,35 @@ import Navbar from "@/pages/layout/Navbar";
 const Upload: React.FC = () => { return (
     <div>
         <Navbar/>
-        <h1>Mediathek</h1>
+        <div className="flex justify-center items-center w-full h-full pt-32">
+            <div className="grid grid-cols-4 grid-rows-2 gap-3">
+                <div className="col-start-1 row-start-1">
+                    <img className="w-96 aspect-video rounded-xl object-cover object-center" src="../imgs/1.png" draggable="false"/>
+                </div>
+                <div className="col-start-2 row-start-1">
+                    <img className="w-96 aspect-video rounded-xl object-cover object-center" src="../imgs/2.png" draggable="false"/>
+                </div>    
+                <div className=" col-start-3 row-start-1">
+                    <img className="w-96 aspect-video rounded-xl object-cover object-center" src="../imgs/3.png" draggable="false"/>
+                </div>    
+                <div className="col-start-4 row-start-1">
+                    <img className="w-96 aspect-video rounded-xl object-cover object-center" src="../imgs/4.png" draggable="false"/>
+                </div>    
+                <div className="col-start-1 row-start-2">
+                    <img className="w-96 aspect-video rounded-xl object-cover object-center" src="../imgs/5.png" draggable="false"/>
+                </div>    
+                <div className="col-start-2 row-start-2">
+                    <img className="w-96 aspect-video rounded-xl object-cover object-center" src="../imgs/6.png" draggable="false"/>
+                </div>    
+                <div className="col-start-3 row-start-2">
+                    <img className="w-96 aspect-video rounded-xl object-cover object-center" src="../imgs/7.png" draggable="false"/>
+                </div>    
+                <div className="rcol-start-4 row-start-2">
+                    <img className="w-96 aspect-video rounded-xl object-cover object-center" src="../imgs/8.png" draggable="false"/>
+                </div>        
+                
+            </div>
+        </div>
     </div>
 );
 };
--- pages/upload.tsx
+++ pages/upload.tsx
@@ -4,7 +4,13 @@ import Navbar from "@/pages/layout/Navbar";
 const Upload: React.FC = () => { return (
     <div>
         <Navbar/>
-        <h1>Upload</h1>
+        <div className="flex justify-center items-center pt-32">
+            <div className="w-1/4 h-96 rounded-xl shadow-xl shadow-gray-800 bg-gray-200 flex flex-col items-center py-8">
+                <h2 className="text-black pt-0 pb-4 my-0">Upload Images</h2>
+                <div className="border-2 border-black border-dashed w-3/4 aspect-square"></div>
+                <span className="self-center text-black pt-2">.png, .jpg</span>
+            </div>
+        </div>
     </div>
 );
 };
