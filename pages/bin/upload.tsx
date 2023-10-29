12fd2c85e8b7cf190db44921e10c30babb344bd3
FinnPenn <finnpenning@hotmail.de>
2023-10-29 23:10:40 +0100

    upload backdrop

--- pages/bin/upload.tsx
+++ pages/bin/upload.tsx
@@ -31,7 +31,7 @@ function nextPage() {
   
   function Upload({ className, id }: UploadProps) {
     return (
-    <div id={`${id || ''}`} className={`bg-dark/90 w-full h-full z-50 absolute ${className || ''}`}>
+    <div id={`${id || ''}`} className={`backdrop-blur-md bg-dark/50 w-full h-full z-50 absolute ${className || ''}`}>
         <div className="flex justify-center items-center pt-32">
             <div className="w-1/4 h-96 rounded-xl shadow-xl shadow-gray-800 bg-white flex flex-col items-center py-8">
             <ModalHeader/>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,6 +16,7 @@ const config: Config = {
       colors: {
         'dark': '#030712',
       },
+    
     },
   },
   plugins: [],
